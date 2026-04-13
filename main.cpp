28d04f5e5e089cb14331f44a80f6c4f5e27184db
Ignas <ignas.andrulionis98@gmail.com>
2026-04-13 11:22:09 +0300

    Prdieti kursai ir pagrindinis pasirinkimu maniu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
-    
+    const double GBP_B = 0.8729, GBP_Pi = 0.8600, GBP_Pa = 0.9220;
+    const double USD_B = 1.1793, USD_Pi = 1.1460, USD_Pa = 1.2340;
+    const double INR_B = 104.6918, INR_Pi = 101.3862, INR_Pa = 107.8546;
+
+    int pasirinkimas, valiuta;
+
+    cout << "1. Kursu palyginimas\n2. Pirkti valiuta\n3. Parduoti valiuta\nPasrinkite: ";
+    cin >> pasirinkimas;
+
+    cout << "pasirinkite valiuta (1_GBP, 2-USD, 3-INR): ";
+    cin >> valiuta;
 
     return 0;
 }
