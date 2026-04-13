e10a566e068b6de349ee3b6ccbd4175ffb7d81c3
Ignas <ignas.andrulionis98@gmail.com>
2026-04-13 11:45:03 +0300

    pridetas kurso pasirinkimas

--- main.cpp
+++ main.cpp
@@ -15,5 +15,20 @@ int main() {
     cout << "pasirinkite valiuta (1_GBP, 2-USD, 3-INR): ";
     cin >> valiuta;
 
+    double kursas = 0;
+    if (valiuta == 1) {
+        if (pasirinkimas == 1) kursas = GBP_B;
+        else if (pasirinkimas == 2) kursas = GBP_Pi;
+        else kursas = GBP_Pa;
+    } else if (valiuta == 2) {
+        if (pasirinkimas == 1) kursas = USD_B;
+        else if (pasirinkimas == 2) kursas = USD_Pi;
+        else kursas = USD_Pa;
+    } else if (valiuta == 3) {
+        if (pasirinkimas == 1) kursas = INR_B;
+        else if (pasirinkimas == 2) kursas = INR_Pi;
+        else kursas = INR_Pa;
+    }
+
     return 0;
 }
